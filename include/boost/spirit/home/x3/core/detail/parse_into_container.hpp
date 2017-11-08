Kojoley/spirit

commit 379413a50c93539aa432fa9f9fc94fec1d6aa60d

Fixed bug where string("string1") >> attr(std::string("string2")) produces the wrong attribute result

include/boost/spirit/home/x3/core/detail/parse_into_container.hpp
@@ -251,7 +251,13 @@ namespace boost { namespace spirit { namespace x3 { namespace detail
           , Iterator& first, Iterator const& last
           , Context const& context, RContext& rcontext, Attribute& attr, mpl::true_)
         {
-            return parser.parse(first, last, context, rcontext, attr);
+            if (attr.empty())
+                return parser.parse(first, last, context, rcontext, attr);
+            Attribute rest;
+            bool r = parser.parse(first, last, context, rcontext, rest);
+            if (r)
+                attr.insert(attr.end(), rest.begin(), rest.end());
+            return r;
         }
 
         template <typename Iterator, typename Attribute>

commit 24ef451ec41f4c2047a718a8151cd2b5cd9c1655

X3: Fix `unused_type` attribute case in `parse_into_container_impl::call`
The issue was introduced in 379413a50c93539aa432fa9f9fc94fec1d6aa60d

include/boost/spirit/home/x3/core/detail/parse_into_container.hpp
@@ -245,6 +245,15 @@ namespace boost { namespace spirit { namespace x3 { namespace detail
                 parser, first, last, context, rcontext, attr);
         }
 
+        template <typename Iterator>
+        static bool call(
+            Parser const& parser
+          , Iterator& first, Iterator const& last
+          , Context const& context, RContext& rcontext, unused_type attr, mpl::true_)
+        {
+            return parser.parse(first, last, context, rcontext, attr);
+        }
+
         template <typename Iterator, typename Attribute>
         static bool call(
             Parser const& parser